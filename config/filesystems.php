38a838fff44f9bab48e2fce4b3829daba9182846
Mehmet Can Orucu <mcorucu@gmail.com>
2026-03-08 21:43:43 +0300

    Configure Cloud SQL (MySQL) and GCS pursuit for data persistence

---
+++ config/filesystems.php
@@ -0,0 +1,52 @@
+<?php
+
+return [
+
+    'default' => env('FILESYSTEM_DISK', 'local'),
+
+    'disks' => [
+
+        'local' => [
+            'driver' => 'local',
+            'root' => storage_path('app'),
+            'throw' => false,
+        ],
+
+        'public' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public'),
+            'url' => env('APP_URL').'/storage',
+            'visibility' => 'public',
+            'throw' => false,
+        ],
+
+        's3' => [
+            'driver' => 's3',
+            'key' => env('AWS_ACCESS_KEY_ID'),
+            'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            'region' => env('AWS_DEFAULT_REGION'),
+            'bucket' => env('AWS_BUCKET'),
+            'url' => env('AWS_URL'),
+            'endpoint' => env('AWS_ENDPOINT'),
+            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
+            'throw' => false,
+        ],
+
+        'gcs' => [
+            'driver' => 'gcs',
+            'key_file_path' => env('GCP_KEY_FILE', null), // path to our service account json file
+            'project_id' => env('GCP_PROJECT_ID'),
+            'bucket' => env('GCP_STORAGE_BUCKET'),
+            'path_prefix' => env('GCP_STORAGE_PATH_PREFIX', null),
+            'storage_api_uri' => env('GCP_STORAGE_API_URI', null),
+            'visibility' => 'public',
+            'throw' => false,
+        ],
+
+    ],
+
+    'links' => [
+        public_path('storage') => storage_path('app/public'),
+    ],
+
+];
